b622dd377d2e249ac09a7e9a646c0566506c1220
MathSena <MathSena07@hotmail.com>
2021-07-31 20:52:50 -0300

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -2,3 +2,17 @@ let js = 'amazing';
 if(js === 'amazing') alert('Javascript s fun!');
 
 console.log(40+30*9-2);
+
+/*Values and Variables*/ 
+console.log("Matheus");
+console.log(23);
+
+let firstName = "Matheus"
+let lastName = "Sena"
+let myFirstJob = "Programmer"
+let myCurrentJob = "Teacher"
+let pi = 3.14
+
+console.log("My name is: " + firstName + " " + lastName)
+console.log("My actually job is: " + myCurrentJob + " and my last job was: " + myFirstJob)
+console.log("And the value of pi is: " + pi)
