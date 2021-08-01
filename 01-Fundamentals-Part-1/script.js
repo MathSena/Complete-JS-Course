22ea082b1262e784bb7365c57694d394fbda692b
MathSena <MathSena07@hotmail.com>
2021-08-01 19:43:59 -0300

    let, const and var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -4,6 +4,8 @@ if(js === 'amazing') alert('Javascript s fun!');
 console.log(40+30*9-2);
 
 /*Values and Variables*/ 
+
+console.log('Values and Variables')
 console.log("Matheus");
 console.log(23);
 
@@ -15,4 +17,25 @@ let pi = 3.14
 
 console.log("My name is: " + firstName + " " + lastName)
 console.log("My actually job is: " + myCurrentJob + " and my last job was: " + myFirstJob)
-console.log("And the value of pi is: " + pi)
+console.log("And the value of pi is: " + pi);
+
+
+// Let, Var e Const
+
+// Let - > Variables that be changed during the program
+
+console.log('Let, Var e Const')
+let age = 30;
+console.log('Before: ' + age)
+age = 50;
+console.log('After: ' + age)
+
+const birthYear = 1996;
+console.log('Before: ' + age)
+birthYear = 1987;
+console.log('After: ' + age)
+
+let job = "Programmer";
+console.log('Before: ' + job)
+job = "Teacher";
+console.log('After: ' + job)
